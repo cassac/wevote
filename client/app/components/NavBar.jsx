1f949b6b59f4672fe7126d65eec539d864a7e944
Hack Reactor Students <accounts@hackreactor.com>
2016-08-30 13:38:36 -0700

    adds tooltip to display voting instructions

--- client/app/components/NavBar.jsx
+++ client/app/components/NavBar.jsx
@@ -5,6 +5,14 @@ let NavBar= ({clickHandler}) => (
     <nav>
       <div className="nav-wrapper blue darken-1">
          <a href="#!" className="brand-logo center"><i className="material-icons">person_pin</i>WeVote</a>
+         <ul id="nav-mobile" className="left">
+           <li>
+            <a href="#" className="tooltipped" data-position="right" data-delay="50" 
+              data-tooltip="Send text to +18988989. Example: #HASHTAG I like voting!">
+              <i className="material-icons">textsms</i>
+            </a>
+        </li>
+         </ul>
         <ul id="nav-mobile" className="right">
           <li onClick={clickHandler}><a href="#"><i className="material-icons">insert_chart</i></a></li>
         </ul>
