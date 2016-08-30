016db5ad16d80034d58dbdf4446a1f6a6d058b6b
Hack Reactor Students <accounts@hackreactor.com>
2016-08-30 11:12:38 -0700

    adds to navbar

--- client/app/components/NavBar.jsx
+++ client/app/components/NavBar.jsx
@@ -5,7 +5,8 @@ let NavBar= () => (
     <nav>
       <div className="nav-wrapper">
          <a href="#!" className="brand-logo center"><i className="material-icons">person_pin</i>WeVote</a>
-        <ul id="nav-mobile" className="hide-on-med-and-down">
+        <ul id="nav-mobile" className="right">
+        <li><a href="#"><i className="material-icons">insert_chart</i></a></li>
         </ul>
       </div>
     </nav>
