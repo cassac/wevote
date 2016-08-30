2089b3cc3126ef590297707d46b4d6fc5ab0c0ba
cassac <hicassac@gmail.com>
2016-08-29 21:40:25 -0700

    adds helper function to extract hashtags

--- server/util/helpers.js
+++ server/util/helpers.js
@@ -1,7 +1,12 @@
 var util = {
   tagGenerator: function(text) {
     var split = text.toUpperCase().split(' ');
-    var hashtag = split[0].concat(split[1]).substring(0, 5);
+    var hashtag = split[0].concat(split[1]).substring(0, 8);
+    return hashtag;
+  },
+  findHashtag: function(body) {
+    var result = body.match(/#([a-zA-Z0-9]+)/);
+    var hashtag = result === null ? null : result[1].toUpperCase();
     return hashtag;
   }
 }
