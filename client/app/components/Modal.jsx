3a2dd3ded66cc35c86364ae91b43ce3dfb5b31e4
Hack Reactor Students <accounts@hackreactor.com>
2016-08-30 12:24:53 -0700

    adds modal

---
+++ client/app/components/Modal.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+let Modal = () => (
+  <div id="modal1" className="modal">
+    <div className="modal-content">
+      <h4>Modal Header</h4>
+      <p>Modal Text</p>
+    </div>
+    <div className="modal-footer">
+      <a href="#!" className=" modal-action modal-close waves-effect waves-green btn-flat">Close</a>
+    </div>
+  </div>
+) 
+
+export default Modal;
