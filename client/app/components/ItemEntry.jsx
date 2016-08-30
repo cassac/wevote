062982b4e77807b987ae3ce4ea88209756a3e868
Hack Reactor Students <accounts@hackreactor.com>
2016-08-30 10:54:14 -0700

    refactors card layout

--- client/app/components/ItemEntry.jsx
+++ client/app/components/ItemEntry.jsx
@@ -2,14 +2,18 @@ import React from 'react';
 
 let ItemEntry = ({item}) => (
 <div className="col s6">
-  <div className="card small horizontal">
+  <div className="card horizontal">
     <div className="card-image waves-effect waves-block waves-light">
-      <img className="activator" src="http://placehold.it/100x100" />
+      <img className="activator" src={item.filepath} />
     </div>
     <div className="card-content">
-      <span className="card-title activator grey-text text-darken-4">#{item.tag}<i className="material-icons right">more_vert</i></span>
-      <p><a href="#">{item.text}</a></p>
-      <p>{item.votes}</p>
+      <span className="card-title activator grey-text text-darken-4">
+        #{item.tag}<i className="material-icons right">more_vert</i>
+      </span>
+      <div>
+        <p>{item.votes}</p>
+        <a href="#">{item.text}</a>
+      </div>
     </div>
     <div className="card-reveal">
       <span className="card-title grey-text text-darken-4">#{item.tag}<i className="material-icons right">close</i></span>
