f27d1c6759f549a4de9c4fb9ac47a1fc0d31984d
cassac <hicassac@gmail.com>
2016-08-29 20:47:10 -0700

    connects vote controller

--- server/routes.js
+++ server/routes.js
@@ -1,12 +1,15 @@
 var ItemController = require('./controllers/ItemController.js');
 var VoteController = require('./controllers/VoteController.js');
 var router = require('express').Router();
+var bodyParser = require('body-parser')
 
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
 
 router.get('/items', ItemController.items.get);
 router.post('/items', ItemController.items.post);
 
-router.post('/votes', VoteController.votes.post);
+router.get('/votes', VoteController.votes.get);
+router.post('/votes', urlencodedParser, VoteController.votes.post);
 
 
 module.exports = router;
