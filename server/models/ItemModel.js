178dcc9f085fa19fb0140210a5bb4f3c89469fae
cassac <hicassac@gmail.com>
2016-09-01 22:16:20 -0700

    uses bluebird for promises. updates schema field type

--- server/models/ItemModel.js
+++ server/models/ItemModel.js
@@ -1,10 +1,11 @@
 var mongoose = require('mongoose');
+mongoose.Promise = require('bluebird');
 
 var itemSchema = mongoose.Schema({
   tag: String,
   text: String,
   filepath: String,
-  votes: {type: Integer, default: 0}
+  votes: {type: Number, default: 0}
 })
 
 module.exports = mongoose.model('Item', itemSchema);
