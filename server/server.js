d7b8b4c06569ea5f00baa0e5b3ead15c2a1e85cb
cassac <hicassac@gmail.com>
2016-09-05 21:06:28 -0700

    adds catch all route

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 var express = require('express');
+var path = require('path');
 var sequelize = require('./db/database.js');
-var router = require('./routes.js')
+var router = require('./routes.js');
 
 var app = express();
 module.exports.app = app;
@@ -9,6 +10,10 @@ app.use('/', router);
 
 app.use(express.static('client'));
 app.use('/static/uploads', express.static('static/uploads'));
+
+app.get('*', (req, res) => {
+	res.redirect('/').sendFile(path.resolve('client', 'index.html'));
+})
 
 var port = process.env.PORT || 8080;
 
